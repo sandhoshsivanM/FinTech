b7c269685f3a396dd3c8536b8a11ac4bbbdc39e1
Sandhosh Sivan <sandhosh.sivan@technoduces.com>
2026-06-30 17:52:58 +0400

    feat: Initialize macOS Flutter project with essential configurations and UI components
    - Added app icon for macOS application.
    - Created MainMenu.xib for the macOS app's main menu interface.
    - Configured AppInfo.xcconfig with application metadata.
    - Set up Debug and Release configuration files for build settings.
    - Established Warnings.xcconfig to enforce strict compiler warnings.
    - Implemented entitlements for Debug and Release builds to enable app sandboxing.
    - Created Info.plist for application properties and metadata.
    - Developed MainFlutterWindow.swift to manage the main application window.
    - Added unit tests in RunnerTests.swift for basic functionality.
    - Included a widget test in widget_test.dart to verify counter functionality.

---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,22 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import file_picker
+import flutter_local_notifications
+import flutter_secure_storage_darwin
+import local_auth_darwin
+import share_plus
+import sqlcipher_flutter_libs
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
+  FlutterSecureStorageDarwinPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageDarwinPlugin"))
+  LocalAuthPlugin.register(with: registry.registrar(forPlugin: "LocalAuthPlugin"))
+  SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
+  Sqlite3FlutterLibsPlugin.register(with: registry.registrar(forPlugin: "Sqlite3FlutterLibsPlugin"))
+}
